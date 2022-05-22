24a197f512a7acd81ab7b2473c24a038d843e74f
Valeria <garkanovavaleria@gmail.com>
2022-05-22 10:39:31 -0400

    test

---
+++ index.js
@@ -0,0 +1,21 @@
+
+const http = require('http'); const fs = require('fs');
+
+const hostname = '127.0.0.1';
+
+const port = 3000;
+
+const home = fs.readFileSync('index.html');
+
+
+const server = http.createServer((req,res)=>{ console.log(req.url);
+
+res.statusCode = 200;
+res.setHeader('Content-Type', 'text/html');
+fs.createReadStream('index.html').pipe(res);
+res.end('hi');
+});
+
+server.listen(port, hostname, () => { console.log(`Server running at http:${hostname}:${port}`); });
+
+
